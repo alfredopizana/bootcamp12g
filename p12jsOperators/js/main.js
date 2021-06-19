eb365aadbf58da30c40a3016bed0c3759973600a
alfredopizana <pizana.alfredo@gmail.com>
2021-06-18 19:02:15 -0500

    Created Folder for JS practice

---
+++ p12jsOperators/js/main.js
@@ -0,0 +1,46 @@
+/*
+1.
+Input: kodemia doceava generación
+Output: 13
+Input: el pájaro dev
+Output: 5
+*/
+/*
+Contar las vocales
+*/
+
+/*
+2.
+Input: Más cuerdo es, el que acepta su propia locura
+Output: 9
+Input: Me volví loco, con largos intervalos de horrible cordura
+Output: 9
+*/
+
+/*
+3.
+Input:Sé verla al revés
+Output: true
+Input: La mañana es fría
+Output: false
+Input:Yo hago yoga hoy
+Output: true
+Input: Y natación mañana
+Output: false
+*/
+
+/*
+4.
+Crear una función que evalúe un string con las siguientes pruebas:
+Tener al menos 8 carácteres
+Tener al menos un número
+Tener al menos una mayúscula
+Devolver true si el string cumple con las reglas, o imprimir en consola la regla o reglas que no se están cumpliendo
+*/
+
+/*
+crear una función que genere un string con "n" caracteres aleatorios,  con la única condición de que no haya caracteres repetidos en el string
+ejemplo correcto:
+input: 10
+output: asdfqwerzx
+*/
