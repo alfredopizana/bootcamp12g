06b1ad479b2a262681ed264e80573845295e4ca7
alfredopizana <pizana.alfredo@gmail.com>
2021-08-06 19:07:52 -0500

    Restful api

--- backend/first-node-api/fileSync.js
+++ backend/first-node-api/fileSync.js
@@ -1,8 +1,10 @@
+const { response } = require('express');
 const express = require('express');
 
 
 const fs = require('fs');
 const server = express();
+server.use(express.json())
 const objectKodemia = JSON.parse(fs.readFileSync('./koders.json'))
 
 function readFilePromise(pathToRead){
@@ -11,7 +13,8 @@ function readFilePromise(pathToRead){
             if(err){
                 reject(err)
             }else{
-                resolve(content)
+                const json  = JSON.parse(content)
+                resolve(json)
             }
         })
     })
@@ -49,8 +52,82 @@ server.get('/read',(req,res)=>{
         res.status(400).json({error:"No se pudo leer archivo"})
     })
 })
+server.get('/koders',async (req,res)=>{
+    const content = await readFilePromise('koders.json')
+    res.status(200).json({
+        success:true,
+        message:'All Koders',
+        data: {
+            koders: content.koders
+        }
+    })
+})
+server.get('/koders/:id',async (request,response)=>{
+    const { id } = request.params
+    const content = await readFilePromise('koders.json')
+    const koder = content.koders.filter(koder=>{
+        return koder.id === parseInt(id)
+    })[0]
+    response.status(200).json({
+        success:true,
+        message:'Your koder is',
+        data: {
+            koder: koder
+        }
+    })
+})
+server.post('/koders',async (request,response)=>{
+    const newKoder = request.body
+    console.log(newKoder)
+    const content = await readFilePromise('koders.json')
+    content.koders.push(newKoder);
+    fs.writeFileSync("koders.json",JSON.stringify(content,null,2),"utf8")
+    response.status(201)
+    response.json({
+        success:true,
+        message:'Koder Added',
+        data: {
+            koder: newKoder
+        }
+    })
+})
+server.patch('/koders/:id',async (request,response)=>{
+    const { id } = request.params
+    const { name, generation } = request.body
+    const content = await readFilePromise('koders.json')
 
+    const newKoders = content.koders.map((koder)=>{
+        if(koder.id === parseInt(id)){
+            if(!name)
+                koder = {...koder,generation};
+            else if(!generation)
+                koder = {...koder,name};
+            else
+                koder = {...koder,name,generation};
+        }
+        return koder
+    })
+    content.koders = newKoders
+    fs.writeFileSync("koders.json",JSON.stringify(content,null,2),"utf8")
+    response.status(200)
+    response.json({
+        success:true,
+        message:'Koder Updated'
+    })
+})
+server.delete("/koders/:id",async (request,response)=>{
+    const { id } = request.params
+    const content = await readFilePromise('koders.json')
+    content.koders = content.koders.filter(koder => {
+        return koder.id != parseInt(id)
+    })
+    fs.writeFileSync("koders.json",JSON.stringify(content,null,2))
+    response.status(200).json({
+        success: true,
+        message: "Koder Deleted"
+    })
+})
 
 server.listen(8080,()=>{
-    console.log("Server Runnin http://locahost:8080");
+    console.log("Server Runnin http://localhost:8080");
 })
